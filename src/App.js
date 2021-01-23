8c222ffd349602c5ea2ab5452f4a87cbda38dac1
BlackDragon-1 <michal.piotr.marczynski@gmail.com>
2021-01-23 13:49:29 +0100

    add structure folders

--- src/App.js
+++ src/App.js
@@ -2,7 +2,42 @@ import './App.css';
 
 function App() {
   return (<>
-    <h1> Hello Ewa !  :D</h1>
+    
+
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>CONTACT</li>
+            <li>LOG IN</li>
+            <li>REGOISTER</li>
+          </ul>
+        </nav>
+      </header>
+      
+
+    <main>
+      <h1>LEISURE TIME WITH CHILD(REN)</h1>
+      <p>Don't waste your time for searching, just use the app and spend it properly!</p>
+
+      <section>
+        <div>outdoor activities</div>
+      </section>
+      
+      <section>
+        <div>indoor activities</div>
+      </section>
+    </main>
+      
+
+    <footer>
+       <div>Follow us</div>
+       <p>All right reserved</p>
+    </footer>
+      
+
+    </div>
     </>
   );
 }
