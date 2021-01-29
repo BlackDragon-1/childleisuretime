e40129144190a4e02bb93907b8a25b38b47d2fe4
BlackDragon-1 <michal.piotr.marczynski@gmail.com>
2021-01-29 23:01:31 +0100

    update database and Fetch API

--- src/components/pages/outdoor/outdoor.js
+++ src/components/pages/outdoor/outdoor.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 
 import MainHeader from '../../atoms/mainHeader';
 import Menu from '../../organism/menu';
@@ -76,101 +76,203 @@ import PageStyle from '../styled/pageStyle';
 
 //==================================
 
-class ActivityOutdoor extends Component {
+// class ActivityOutdoor extends Component {
 
-    
+//     state={
+//         atr: "",
+//         text:"strona",
+//         error:''
+//     }
 
-    state={
-        atr: "",
-        text:"strona",
-        error:''
-    }
+//     // handleAtr=(e)=>{ 
 
-    // handleAtr=(e)=>{ 
+//     //     const random = e.target.value;
 
-    //     const random = e.target.value;
+//     //     fetch(`http://localhost:3004/atractive/`
+//     //         .then(res=>{
+//     //             if(res.ok){
+//     //                 return res;
+//     //             }throw Error(res.status)
+//     //         })
 
-    //     fetch(`http://localhost:3004/atractive/`
-    //         .then(res=>{
-    //             if(res.ok){
-    //                 return res;
-    //             }throw Error(res.status)
-    //         })
+//     //         .then(res => res.json())
 
-    //         .then(res => res.json())
+//     //         .then(objektJSON => this.setState({
+//     //             atr: objektJSON.atraction
+//     //         }))
 
-    //         .then(objektJSON => this.setState({
-    //             atr: objektJSON.atraction
-    //         }))
-
-    //         .catch(err => console.log(err))    
+//     //         .catch(err => console.log(err))    
         
-    // }
+//     // }
 
-    handleDateChange=(e)=>{ 
-        e.preventDefault();
-        const random = e.target.value;
+//     handleDateChange=(e)=>{ 
+//         e.preventDefault();
+//         const random = e.target.value;
         
-        fetch(`http://numbersapi.com/${random}/year?json`)
-            .then(res=>
+//         fetch(`http://numbersapi.com/${random}/year?json`)
+//             .then(res=>        
+//                 {
+//                 if(res.ok){
+//                     return res;
+//                 }throw Error(res.status)
+//                 }
+//             )
+
+//             .then(res => res.json())
+
+//             .then(objektJSON => {
+//                 console.log(objektJSON.text);
+//                 this.setState({
+//                 text: objektJSON.text
+//                 })
+//             })
+
+//             .catch(err => console.log(err));              
+//     }
+
+
+//     render(){
+//     return (
+//         <>
+//             <PageStyle>
+//                 <Menu/>
+//                 <MainHeader/>
+//                 <Form/>
+//                 <div>pusty div</div>
+//                 <div>wprowadź rok:</div>
+//                 <input  onChange={this.handleDateChange} type="text"/>
+//                 <h3>POBRANE Z API</h3>
+//                 <h4>{this.state.text}</h4>
+
+//                 <p>Provide the age range of your child</p>
+
+
+//                 <button onClick={this.handleDateChange}>zobacz zdanie</button>
+
+
+//             </PageStyle>
+//         </>
+//     );
+//     }
+// };
+
+// export default ActivityOutdoor;
+
+//======================================
+
+// class ActivityOutdoor extends Component {
+
+//     state={
+//         atraction:'tu będzie atrakcja'
+//     }
+
+//     // handleAtr=(e)=>{ 
+
+//     //     const random = e.target.value;
+
+//     //     fetch(`http://localhost:3004/atractive/`
+//     //         .then(res=>{
+//     //             if(res.ok){
+//     //                 return res;
+//     //             }throw Error(res.status)
+//     //         })
+
+//     //         .then(res => res.json())
+
+//     //         .then(objektJSON => this.setState({
+//     //             atr: objektJSON.atraction
+//     //         }))
+
+//     //         .catch(err => console.log(err))    
+        
+//     // }
+
+//     handleDateChange=(e)=>{ 
+        
+        
+//         fetch(`http://localhost:3004/attractions`)
+
+//             .then(res => res.json())
+
+//             .then(objektJSON => {
+//                 console.log(objektJSON.atraction);
+//                 this.setState({
+//                     atraction: objektJSON.atraction
+//                 })
+//             })
+
+//             .catch(err => console.log(err));              
+//     }
+
+
+//     render(){
+//     return (
+//         <>
+//             <PageStyle>
+//                 <Menu/>
+//                 <MainHeader/>
+//                 <Form/>
+//                 <div>pusty div</div>
+//                 <div>Pobierz atrakcję</div>
                 
-                {
-                if(res.ok){
-                    return res;
-                }throw Error(res.status)
-            }
-            )
+//                 <h3>POBRANE Z API:</h3>
 
-            .then(res => res.json())
+//                 <h4>{this.state.atraction}</h4>
 
-            .then(objektJSON => console.log(objektJSON.text)
+//                 <button onClick={this.handleDateChange}>pobierz</button>
+
                 
-            //     this.setState({
-            //     text: objektJSON.text
-            // })
-            
-            )
+//             </PageStyle>
+//         </>
+//     );
+//     }
+// };
 
-            .catch(err => console.log(err));        
-        
-    }
-    
+// export default ActivityOutdoor;
 
-    render(){
-    return (
+
+//=====================================================================================const places = () => {
+    const ActivityOutdoor = () => {
+
+        const [places, setPlaces] = useState([]);
+        const [city, setCity] = useState("PL");
+      
+        useEffect(() => {
+      
+          fetch(`http://localhost:3004/attractions`)
+            .then((res) => res.json())
+            .then((data) => {
+              setPlaces(data); 
+            })
+            .catch((err) => console.log(err));
+             
+        }, [city]);
+      
+        return (
         <>
             <PageStyle>
-                <Menu/>
-                <MainHeader/>
-                <Form/>
-                <div>pusty div</div>
-                <div>wprowadź rok:</div>
-                <input  onChange={this.handleDateChange} type="text"/>
-                <h3>POBRANE Z API</h3>
-                <h4>{this.state.text}</h4>
+            <Menu/>
+            <MainHeader/>
+          <div>
+            <select value={city} onChange={(e) => setCity(e.target.value)}>
+              <option value="Wrocław">Wrocław</option>
+              <option value="Poznań">Poznań</option>
+              <option value="Kraków">Kraków</option>
+            </select>
 
-                <p>Provide the age range of your child</p>
-        
-                {/* <select value={age} onChange={(e) => setAge(e.target.value)}>  // jak to przepisać na komponennt funkcyjny?
-                    <option value="set age range">  set age range</option>
-                    <option value="0-2">0-2</option>
-                    <option value="3-6">3-6</option>
-                    <option value="7-13">7-13</option>
-                    <option value="14-17">14-17</option>
-                    <option value="over18">18</option>
-                </select>
-
-                <span>{age}</span>   */}
-
-
-                <button onClick={this.handleDateChange}>zobacz zdanie</button>
-
-                {/* <ButtonActivityOutdoor/>
-                <Rights year="2021"/>             */}
-            </PageStyle>
+            <ul>
+               {places.map((el) => {
+                        return (
+                            <li key={el.id}>
+                                <h3>{el.holidays}</h3>
+                            </li>
+                        );
+                })}
+            </ul>
+          </div>
+        </PageStyle>
         </>
-    );
-    }
-};
-
+        );
+      };
+      
 export default ActivityOutdoor;
